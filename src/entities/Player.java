711cab0881dc0bc538ff58515cd2696deb490f60
Htifano <91378921+Kuretha@users.noreply.github.com>
2023-05-10 14:34:00 +0700

    Saving Changes

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -2,7 +2,9 @@ package entities;
 
 import static utilz.Constants.PlayerConstants.*;
 import static utilz.HelpMethods.*;
-import java.awt.Graphics;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
 import main.Game;
@@ -28,22 +30,78 @@ public class Player extends Entity{
 	private float fallSpeedAfterCollision = 0.5f * Game.SCALE;
 	private boolean inAir = false;
 
+	//Status Bar UI
+	private BufferedImage statusBarImage;
+
+	private int statusBarWidth = (int)(192 * Game.SCALE);
+	private int statusBarHeight = (int)(58 * Game.SCALE);
+	private int statusBarX = (int)(10 * Game.SCALE);
+	private int statusBarY = (int)(10 * Game.SCALE);
+	private int healthBarWidth = (int)(150 * Game.SCALE);
+	private int healthBarHeight = (int)(4 * Game.SCALE);
+	private int healthBarXStart = (int)(34 * Game.SCALE);
+	private int healthBarYStart = (int)(14 * Game.SCALE);
+
+	private int maxHealth = 100;
+	private int currentHealth = 40;
+	private int healthWidth = healthBarWidth;
+
+	//Attack Box
+	private Rectangle2D.Float attackBox;
+
+	private  int flipX = 0;
+	private  int flipW = 1;
+
 	public  Player(float x, float y, int width, int height){
 		super(x, y, width, height);
 		loadAnimations();
 		initHitbox(x, y, (int)(20 * Game.SCALE), (int)(27 * Game.SCALE));
+		initAttackBox();
+	}
+
+	private void initAttackBox() {
+		attackBox = new Rectangle2D.Float(x, y, (int)(20 * Game.SCALE), (int)(20 * Game.SCALE));
 	}
 
 	public void update(){
+		updateHealthBar();
+		updateAttackBox();
+
 		updatePos();
 		updateAnimationTick();
 		setAnimation();
 
 	}
 
+	private void updateAttackBox() {
+		if (right) {
+			attackBox.x = hitbox.x + hitbox.width + (int) (Game.SCALE * 10);
+		} else if (left) {
+			attackBox.x = hitbox.x - hitbox.width - (int) (Game.SCALE * 10);
+		}
+		attackBox.y = hitbox.y + (Game.SCALE * 10);
+	}
+
+	private void updateHealthBar() {
+		healthWidth = (int)((currentHealth / (float)(maxHealth)) * healthBarWidth);
+	}
+
 	public void render(Graphics g, int lvlOffset){
 		g.drawImage(animations[playerAction][aniIndex], (int)(hitbox.x - xDrawOffset) - lvlOffset, (int)(hitbox.y - yDrawOffset),  width, height, null);
-//		drawHitbox(g);
+//		drawHitbox(g, lvlOffset);
+		drawAttackBox(g, lvlOffset);
+		drawUI(g);
+	}
+
+	private void drawAttackBox(Graphics g, int lvlOffsetX) {
+		g.setColor(Color.red);
+		g.drawRect((int) attackBox.x - lvlOffsetX, (int)attackBox.y, (int)attackBox.width, (int)attackBox.height);
+	}
+
+	private void drawUI(Graphics g) {
+			g.drawImage(statusBarImage, statusBarX, statusBarY, statusBarWidth, statusBarHeight, null);
+			g.setColor(Color.red);
+			g.fillRect(healthBarXStart + statusBarX, healthBarYStart + statusBarY, healthWidth, healthBarHeight);
 	}
 
 	private void updateAnimationTick() {
@@ -75,7 +133,7 @@ public class Player extends Entity{
 		}
 
 		if (attacking)
-			playerAction = ATTACK_1;
+			playerAction = ATTACK;
 
 		if (startAni != playerAction)
 			resetAniTick();
@@ -137,6 +195,16 @@ public class Player extends Entity{
 		airSpeed = 0;
 	}
 
+	public void changeHealth(int value) {
+		currentHealth += value;
+
+		if (currentHealth <= 0) {
+			currentHealth = 0;
+//			gameover();
+		} else if (currentHealth >= maxHealth)
+			currentHealth = maxHealth;
+	}
+
 	private void updateXPos(float xSpeed) {
 		if (CanMoveHere(hitbox.x+xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData)){
 			hitbox.x += xSpeed;
@@ -149,10 +217,12 @@ public class Player extends Entity{
 
 			BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
 
-			animations = new BufferedImage[9][6];
+			animations = new BufferedImage[7][8];
 			for (int j = 0; j < animations.length; j++)
 				for (int i = 0; i < animations[j].length; i++)
 					animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
+
+			statusBarImage = LoadSave.GetSpriteAtlas(LoadSave.STATUS_BAR);
 	}
 
 	public void loadlvlData(int[][] lvlData){
--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -1,15 +1,21 @@
 
 package gamestates;
 
+import entities.EnemyManager;
 import entities.Player;
 import levels.LevelManager;
 import main.Game;
 import ui.PauseOverlay;
 import utilz.LoadSave;
 
-import java.awt.*;
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import java.util.Random;
+
+import static utilz.Constants.Environment.*;
 
 import static main.Game.GAME_WIDTH;
 import static main.Game.SCALE;
@@ -17,6 +23,7 @@ import static main.Game.SCALE;
 public class Playing extends State implements Statemethods{
 	private Player player;
 	private LevelManager levelManager;
+	private EnemyManager enemyManager;
 	private PauseOverlay pauseOverlay;
 	private boolean paused = false;
 
@@ -27,14 +34,25 @@ public class Playing extends State implements Statemethods{
 	private int maxTilesOffset = lvlTilesWide - Game.TILES_IN_WIDTH;
 	private int maxLvlOffsetX = maxTilesOffset * Game.TILES_SIZE;
 
+	private BufferedImage backgroundImg, bigCloud, smallCloud;
+	private int[] smallCloudsPos;
+	private Random rnd = new Random();
 
 	public Playing(Game game) {
 		super(game);
 		initClasses();
+
+		backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.PLAYING_BG_IMG);
+		bigCloud = LoadSave.GetSpriteAtlas(LoadSave.BIG_CLOUDS);
+		smallCloud = LoadSave.GetSpriteAtlas(LoadSave.SMALL_CLOUDS);
+		smallCloudsPos = new int[8];
+		for (int i = 0; i < smallCloudsPos.length; i++)
+			smallCloudsPos[i] = (int)(90 * Game.SCALE) + rnd.nextInt((int) (100 * Game.SCALE));
 	}
 
 	private void initClasses() {
 		levelManager = new LevelManager(game);
+		enemyManager = new EnemyManager(this);
 		player = new Player(200, 200, (int)(64 * Game.SCALE), (int)(40 * Game.SCALE));
 		player.loadlvlData(levelManager.getCurrentLevel().getLvlData());
 		pauseOverlay = new PauseOverlay(this);
@@ -45,6 +63,7 @@ public class Playing extends State implements Statemethods{
 		if (!paused) {
 			levelManager.update();
 			player.update();
+			enemyManager.update(levelManager.getCurrentLevel().getLvlData(), player);
 			checkCloseToBorder();
 		} else {
 			pauseOverlay.update();
@@ -67,14 +86,27 @@ public class Playing extends State implements Statemethods{
 
 	@Override
 	public void draw(Graphics g) {
+		g.drawImage(backgroundImg, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
+
+		drawClouds(g);
+
 		levelManager.draw(g, xLvlOffset);
 		player.render(g, xLvlOffset);
+		enemyManager.draw(g, xLvlOffset);
 
 		if (paused) {
 			g.setColor(new Color(0, 0,0,180));
 			g.fillRect(0, 0, Game.GAME_WIDTH,Game.GAME_HEIGHT);
 			pauseOverlay.draw(g);
 		}
+	}
+
+	private void drawClouds(Graphics g) {
+
+		for (int i = 0; i < 3 ;i++)
+			g.drawImage(bigCloud, i * BIG_CLOUDS_WIDTH - (int)(xLvlOffset * 0.3), (int)(204 * Game.SCALE), BIG_CLOUDS_WIDTH, BIG_CLOUDS_HEIGHT, null);
+		for (int i = 0; i < smallCloudsPos.length ; i++)
+			g.drawImage(smallCloud, SMALL_CLOUDS_WIDTH * 4 * i -(int)(xLvlOffset * 0.7) , smallCloudsPos[i], SMALL_CLOUDS_WIDTH, SMALL_CLOUDS_HEIGHT, null);
 	}
 
 	@Override
